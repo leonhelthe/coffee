8221231154990c7cb4d8f7421e30c4e30d781a2d
john loenhel ang <johnleonhelang@gmail.com>
2019-02-03 15:10:01 +0800

    json test

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const app = angular.module("myApp", []);
 
-
-
-
-
+app.controller("myController", function($scope, $http){
+   $scope.coffees = [];
+   $http.get('./coffee.json').then(function(data){
+      $scope.coffees = data.data;
+   })
+});
